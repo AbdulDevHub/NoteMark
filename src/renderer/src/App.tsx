f2e08b07f85f4752ef386db89948a7ba32363a36
Abdul Khan <ahkn63@gmail.com>
2024-04-29 18:20:54 -0400

    Initial commit

---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex h-full items-center justify-center">
+      <span className="text-4xl">Hello from Electron</span>
+    </div>
+  )
+}
+
+export default App
